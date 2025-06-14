ce2d1b68b18b1ce806d168bd2ab12e1b99ba98f1
SARAVANAN <waransaravanan@gmail.com>
2025-06-14 13:05:09 +0530

    POJO

--- Employee.java
+++ Employee.java
@@ -1,1 +1,33 @@
+class Employee
+{
+private int emp_num;
+private String emp_name:
+private String emp_location;
 
+public Employee(int emp_name, String emp_name, String emp_location){
+  this.emp_num = emp_name;
+  this.emp_name = emp_name;
+  this.emp_location = emp_location;
+}
+  public int getEmp_Num(){
+    return this.emp_num;
+  }
+  public void setEmp_num(int emp_num){
+    this.emp_num = emp_num;
+  }
+
+  public String getEmp_Name(){
+    return this.emp_name;
+  }
+  public void setEmp_Name(){
+    this.emp_name = emp_name;
+  }
+
+  public String getEmp_Location(){
+    return this.emp_location;
+  }
+  public void setEmp_Location(String emp_location){
+    this.emp_location = emp_location;
+  }
+  
+}
