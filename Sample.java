c73c12dfe7e45a8d15fb3c4b94d0735a13e94fba
SARAVANAN <waransaravanan@gmail.com>
2025-06-14 13:20:26 +0530

    Collections tutorial

--- Sample.java
+++ Sample.java
@@ -3,5 +3,19 @@ class Sample
 public static void main(String args[])
 {
   System.out.println("Welcome");
+  Employee emp;
+  emp =  new Employee(100,"Stuart","NewYork");
+  emp =  new Employee(104,"David","Melbourne");
+  emp =  new Employee(101,"Ruther","Jamaica");
+  emp =  new Employee(103,"James","London");
+  emp =  new Employee(105,"Kane","Barcelona");
+  emp =  new Employee(102,"John","Pretoria");
+  
+
+  System.out.println(emp);
+
+  Collections.sort(emp);
+
+  System.out.println(emp);
 }
 }
